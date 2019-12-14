2956999797e68456ac6a95182849fb2b8be20e67
Luke Childs <lukechilds123@gmail.com>
2019-12-14 18:39:22 +0700

    Throw helpful error if no filesystem image exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,15 @@
 image_path="/sdcard/filesystem.img"
 zip_path="/filesystem.zip"
 
-if [ ! -e $image_path ] && [ -e $zip_path ]; then
-    echo "No filesystem detected at ${image_path}!"
-    echo "Extracting..."
-    unzip $zip_path
-    mv *.img $image_path
+if [ ! -e $image_path ]; then
+  echo "No filesystem detected at ${image_path}!"
+  if [ -e $zip_path ]; then
+      echo "Extracting fresh filesystem..."
+      unzip $zip_path
+      mv *.img $image_path
+  else
+    exit 1
+  fi
 fi
 
 exec qemu-system-arm \
