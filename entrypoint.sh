ab7c78c06be183505314f72613e94b5fcf1f94c5
Luke Childs <lukechilds123@gmail.com>
2021-05-16 16:19:31 +0700

    Use consistent string quoting

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,23 +29,23 @@ if [ "${target}" = "pi1" ]; then
   machine=versatilepb
   memory=256m
   root=/dev/sda2
-  nic='--net nic --net user,hostfwd=tcp::5022-:22'
+  nic="--net nic --net user,hostfwd=tcp::5022-:22"
 elif [ "${target}" = "pi2" ]; then
   emulator=qemu-system-arm
   machine=raspi2b
   memory=1024m
   kernel_pattern=kernel7.img
   dtb_pattern=bcm2709-rpi-2-b.dtb
-  append='dwc_otg.fiq_fsm_enable=0'
-  nic='-netdev user,id=net0,hostfwd=tcp::5022-:22 -device usb-net,netdev=net0'
+  append="dwc_otg.fiq_fsm_enable=0"
+  nic="-netdev user,id=net0,hostfwd=tcp::5022-:22 -device usb-net,netdev=net0"
 elif [ "${target}" = "pi3" ]; then
   emulator=qemu-system-aarch64
   machine=raspi3b
   memory=1024m
   kernel_pattern=kernel8.img
   dtb_pattern=bcm2710-rpi-3-b-plus.dtb
-  append='dwc_otg.fiq_fsm_enable=0'
-  nic='-netdev user,id=net0,hostfwd=tcp::5022-:22 -device usb-net,netdev=net0'
+  append="dwc_otg.fiq_fsm_enable=0"
+  nic="-netdev user,id=net0,hostfwd=tcp::5022-:22 -device usb-net,netdev=net0"
 else
   echo "Target ${target} not supported"
   echo "Supported targets: pi1 pi2 pi3"
